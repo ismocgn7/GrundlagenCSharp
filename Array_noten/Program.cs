8885eb95e47dd9bd961969e1d245bbf89524b10b
Ismael Zanati <ismael.z2@gso.schule.koeln>
2022-03-10 10:01:18 +0100

    Test

---
+++ Array_noten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Array_noten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
