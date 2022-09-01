3b8b7ea0e494a6ecc0e869f46e3443eab6305616
Ismael Zanati <ismael.z2@gso.schule.koeln>
2022-09-01 11:12:45 +0200

    Aufagabe 1 und 2 eingefügt

---
+++ 2EingabeAusgabe/Program.cs
@@ -0,0 +1,21 @@
+﻿//A1
+Console.WriteLine(" _-_ ");
+Console.WriteLine("(o o)");
+Console.WriteLine("| 0 |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
+
+
+//A2
+string Vorname = "Ismael";
+string Nachname = "Zanati";
+string Alter = "19";
+
+
+
+
+Console.WriteLine("Vorname: {0}", Vorname);
+Console.WriteLine("Nachname: {0}", Nachname);
+Console.WriteLine("Alter: {0}", Alter);
+
+//A3
