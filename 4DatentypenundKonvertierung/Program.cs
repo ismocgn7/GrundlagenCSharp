5811b04d13274f31b6549ab1dabd19979c8d8a3c
Ismael Zanati <ismael.z2@gso.schule.koeln>
2022-09-08 10:55:49 +0200

    DatentypenundKonvertierung Aufgabe 1, 2, 3 und 4

---
+++ 4DatentypenundKonvertierung/Program.cs
@@ -0,0 +1,50 @@
+﻿//A1
+Console.WriteLine(short.MaxValue);
+Console.WriteLine(short.MinValue);
+/*
+    16 Bit
+    32767
+    -32767
+  */
+
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+/*
+    * 2147483647
+    -2147483648
+    */
+
+
+//A2
+string vorname;
+string alter;
+string frage;
+
+Console.WriteLine("Ich stelle Ihnen jetzt éin Paar Fragen.");
+Console.WriteLine("");
+Console.WriteLine("1. Wie heißen Sie?");
+vorname = Console.ReadLine();
+Console.WriteLine("2. Wie alt sind Sie?");
+alter = Console.ReadLine();
+Console.WriteLine("3. Ist C# die erste Programiersprache, die Sie lernen?");
+frage = Console.ReadLine();
+
+Console.WriteLine("");
+Console.WriteLine("Ihre Antworten:");
+Console.WriteLine($"1. {vorname}\n2. {alter}\n3. {frage}");
+
+
+
+//A3
+/*
+ int Dollarzahl = 36;
+ char Dollar = Dollarzahl;
+ */
+
+
+
+//A4
+Console.WriteLine("Geben sie die erste Variable ein!");
+string v1_str = Console.ReadLine();
+
+short v1_str = Convert.ToInt16(v1_str);
